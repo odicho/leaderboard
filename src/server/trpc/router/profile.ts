ebfc6893cdbcfa1f79f8ea939426a4691c982b3b
Amanuel Odicho <53154523+odicho@users.noreply.github.com>
2023-02-02 00:06:29 -0800

    Removed unused imports

--- src/server/trpc/router/profile.ts
+++ src/server/trpc/router/profile.ts
@@ -1,6 +1,4 @@
-import { Run } from "@prisma/client";
 import { z } from "zod";
-import { prisma } from "../../../server/db/client";
 import { getProfile } from "../../common/get-profile";
 import { router, publicProcedure } from "../trpc";
 
