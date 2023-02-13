962a283cc76b6f52c66e24bc8447d5e273cf8a5a
Amanuel Odicho <53154523+odicho@users.noreply.github.com>
2023-02-12 19:38:52 -0800

    Fixed bug of max size for calendar on sm+

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -99,7 +99,7 @@ const MonthBlock = ({
 		<div className="absolute z-10 pt-12 sm:right-0">
 			<div
 				ref={modalRef}
-				className="w-screen max-w-[50rem] rounded-md border bg-[#FFFFFF] p-2 shadow-md sm:min-w-[400px]"
+				className="w-screen rounded-md border bg-[#FFFFFF] p-2 shadow-md sm:min-w-[400px] sm:max-w-[20rem]"
 			>
 				<div className="flex flex-col gap-2">
 					<div className="flex items-center justify-between gap-3">
